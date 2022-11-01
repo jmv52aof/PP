f09d28e63dddb4cdc0d690772275412dcb92f544
jmv52aof <100315736+jmv52aof@users.noreply.github.com>
2022-11-01 16:26:02 +0300

    Add files via upload

---
+++ 01_11/task1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <ctype.h>
+
+using namespace std;
+
+void print_vector(vector<int> v)
+{
+	copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+	cout << endl;
+}
+
+int main()
+{
+	vector<int> numbers = { 8, 2, 3, 4, 5, 6, 7, 8 };
+
+	vector<int> swings = {};
+	transform(
+		numbers.begin(), 
+		numbers.end(),
+		back_inserter(swings),
+		[max_elem = 0, min_elem = INT_MAX] (int item) mutable {
+			max_elem = max(max_elem, item);
+			min_elem = min(min_elem, item);
+			return max_elem - min_elem;
+		}
+	);
+	print_vector(swings);
+}
